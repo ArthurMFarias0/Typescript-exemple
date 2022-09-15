190368a326b611ec29bed207992206a377b75b7f
Arthur <arthuremeunome@gmail.com>
2022-09-15 15:32:53 -0300

    TypeScript exemple

---
+++ src/script.ts
@@ -0,0 +1,10 @@
+const idade: number = 27;
+const nome: string = 'Arthur';
+const maiorDeIdade: boolean = true;
+
+function soma (num1: number, num2: number): number {
+    const soma: number = num1 + num2
+    return soma
+}
+
+document.write(`A soma de 7 + 5 é ${soma(7,5)}`)
